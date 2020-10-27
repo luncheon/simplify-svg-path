4438af8d25045770563120f4f307a3ab5bb82b21
luncheon <takeuck+github@gmail.com>
2020-10-27 12:58:21 +0900

    fix test

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 import assert from 'assert'
 import Paper from 'paper'
-import simplifySvgPath from './index.js'
+import simplifySvgPath from './index.min.js'
 
 new Paper.Project()
 
-const simplifySvgPathByPaper = segments => {
+const simplifySvgPathByPaper = (segments, tolerance) => {
   const path = new Paper.Path(segments)
-  path.simplify(1)
+  path.simplify(tolerance)
   return path.pathData
 }
 
-const points = Array(100).fill(0).map(() => [Math.random() * 100, Math.random() * 100])
-assert.strictEqual(simplifySvgPath(points), simplifySvgPathByPaper(points))
+for (let i = 0; i < 100; i++) {
+  const points = Array(100).fill(0).map(() => [Math.random() * 100, Math.random() * 100])
+  const tolerance = Math.random() * 5
+  assert.strictEqual(simplifySvgPath(points, { tolerance }), simplifySvgPathByPaper(points, tolerance))
+}
 
 console.log('passed')
