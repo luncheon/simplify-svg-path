383e72ffd8e7aeb99a88cc84c2fe227fbc2327c5
luncheon <takeuck+github@gmail.com>
2022-05-28 14:43:14 +0900

    measure metrics during build and test

--- build-iife.js
+++ build-iife.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
+import { gzipSync } from 'zlib'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -8,3 +9,4 @@ const __dirname = dirname(__filename)
 const esm = fs.readFileSync(resolve(__dirname, 'index.min.js'), 'utf8')
 const iife = 'var simplifySvgPath=(function(){' + esm.replace('export default', 'return') + '})()'
 fs.writeFileSync(resolve(__dirname, 'index.iife.min.js'), iife, 'utf8')
+console.log('index.iife.min.js', iife.length, 'bytes, gzip', gzipSync(iife, { level: 9 }).byteLength, 'bytes')
--- test.js
+++ test.js
@@ -11,21 +11,31 @@ const simplifySvgPathByPaper = (segments, { closed, tolerance, precision }) => {
   return path.getPathData(undefined, precision)
 }
 
-for (let i = 0; i < 1000; i++) {
+let actualTime = 0
+let expectedTime = 0
+
+for (let i = 0; i < 100; i++) {
   const points = []
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     points.push([Math.random() * 100, Math.random() * 100])
   }
   const options = {
     closed: Math.random() < 0.5,
     tolerance: Math.random() * 5 || 2.5,
-    precision: (Math.random() * 20) | 0,
+    precision: i % 4,
   }
+  const now1 = performance.now()
+  const actual = simplifySvgPath(points, options)
+  const now2 = performance.now()
+  const expected = simplifySvgPathByPaper(points, options)
+  const now3 = performance.now()
+  actualTime += now2 - now1
+  expectedTime += now3 - now2
   assert.strictEqual(
-    simplifySvgPath(points, options),
-    simplifySvgPathByPaper(points, options),
+    actual,
+    expected,
     `simplified path does not equal to Paper.js. closed: ${options.closed}, tolerance: ${options.tolerance}, precision: ${options.precision}`,
   )
 }
 
-console.log('passed')
+console.log('passed', actualTime, '[ms]', Math.round((actualTime / expectedTime) * 10000) / 100, '%')
